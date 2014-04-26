d2287898d118d501904711c118aa23016572cf12
Julian C. Dunn <jdunn@aquezada.com>
2014-04-26 12:35:19 -0400

    Update metadata for release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jdunn@aquezada.com'
 license          'Apache 2.0'
 description      'Installs Atlassian HipChat client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          '1.1.2'
 
 %w{mac_os_x windows debian ubuntu centos rhel}.each do |os|
   supports os
