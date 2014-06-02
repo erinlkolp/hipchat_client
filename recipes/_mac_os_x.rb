7e86e6274c9bbff5cb89712750a11b6ef7cfe449
Drewzar <drew@drewzar.com>
2014-06-02 15:52:39 -0700

    dont use https

--- recipes/_mac_os_x.rb
+++ recipes/_mac_os_x.rb
@@ -18,7 +18,7 @@
 #
 
 remote_file "#{Chef::Config[:file_cache_path]}/HipChat-2.5.6-87.zip" do
-  source 'https://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.5.6-87.zip'
+  source 'http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.5.6-87.zip'
   checksum '8d764118f93efed5e6fc61dffa00da26d946a84a9e35ca2e07babdaa075760db'
   notifies :run, 'execute[unzip-hipchat]'
 end
