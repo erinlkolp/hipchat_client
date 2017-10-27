95c20de35676c3d79e00d250ce1d8c8f97039b82
Erin Kolp <ekolp@kickbackpoints.com>
2017-10-27 17:42:29 -0600

    Linux updated.

--- recipes/_linux.rb
+++ recipes/_linux.rb
@@ -3,26 +3,32 @@
 # Recipe:: linux
 #
 
-case node['platform_family']
-when 'debian'
-  apt_repository node['hipchat']['repo_name'] do
-    action     :add
-    components ['stable', 'main']
-    key        node['hipchat']['key']
-    repo_name  "#{node['hipchat']['repo_name']}.list"
-    uri        node['hipchat']['uri']
-  end
-when 'rhel'
-  yum_repository node['hipchat']['repo_name'] do
-    action      :create
-    baseurl     node['hipchat']['baseurl']
-    description node['hipchat']['description']
-    enabled     true
-    gpgcheck    true
-    gpgkey      node['hipchat']['key']
-  end
-end
+CODENAME = node['lsb']['codename']
 
-package 'hipchat' do
-  action :install
+case node['platform']
+when 'debian', 'ubuntu'
+  if CODENAME == 'trusty' || CODENAME == 'xenial'
+    apt_repository node['hipchat_client']['repo_name'] do
+      uri         node['hipchat_client']['uri']
+      key         node['hipchat_client']['key']
+      components  ['main']
+      action :add
+    end
+
+    package 'hipchat4' do
+      action :install
+    end
+  end
+when 'redhat', 'centos', 'fedora'
+  yum_repository node['hipchat_client']['repo_name'] do
+    description node['hipchat_client']['description']
+    baseurl node['hipchat_client']['baseurl']
+    gpgkey node['hipchat_client']['key']
+    action :create
+  end
+
+  yum_package 'hipchat4' do
+    options '--nogpgcheck'
+    action :install
+  end
 end
