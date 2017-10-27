88d3fa573be2f473f5b793c1c36059da82fbea56
Erin Kolp <ekolp@kickbackpoints.com>
2017-10-27 15:20:10 -0600

    Update package urls and checksums

--- recipes/_mac_os_x.rb
+++ recipes/_mac_os_x.rb
@@ -17,13 +17,16 @@
 # limitations under the License.
 #
 
-remote_file "#{Chef::Config[:file_cache_path]}/HipChat-2.5.6-87.zip" do
-  source 'http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.5.6-87.zip'
-  checksum '8d764118f93efed5e6fc61dffa00da26d946a84a9e35ca2e07babdaa075760db'
+HC_OSX_VERSION = '4.29.0-732'
+HC_OSX_CHECKSUM = 'b033c8e69686810e7f83e0895a90ed22d745af5956b5bb1cf4cda37756dc910b'
+
+remote_file "#{Chef::Config[:file_cache_path]}/HipChat-#{HC_OSX_VERSION}.zip" do
+  source "https://s3.amazonaws.com/downloads.hipchat.com/osx/HipChat-#{HC_OSX_VERSION}.zip"
+  checksum "#{HC_OSX_CHECKSUM}"
   notifies :run, 'execute[unzip-hipchat]'
 end
 
 execute 'unzip-hipchat' do
-  command "unzip #{Chef::Config[:file_cache_path]}/HipChat-2.5.6-87.zip -d /Applications"
+  command "unzip #{Chef::Config[:file_cache_path]}/HipChat-#{HC_OSX_VERSION}.zip -d /Applications"
   action :nothing
 end
